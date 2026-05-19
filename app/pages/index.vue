d50de207025904bff268ddb606c3cee70632082d
unknown <colisliezelann2022@gmail.com>
2026-05-19 10:25:21 +0800

    navigation drawer setup

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,17 +5,45 @@
                <v-row>
                     <v-col>
                         <v-text-field
-                        label="STUDENT NO."
+                            label="STUDENT NO."
+                            variant="outlined">
+                        </v-text-field>
+                    </v-col>
+               </v-row>
+
+               <v-row>
+                    
+                    <v-col
+                    cols="12"
+                    md="4">
+                        <v-text-field
+                            label="LAST NAME"
+                            variant="outlined">
+                        </v-text-field>
+                    </v-col>
+
+                    <v-col
+                    cols="12"
+                    md="4">
+                        <v-text-field
+                        label="FIRST NAME"
                         variant="outlined">
                         </v-text-field>
                     </v-col>
 
-                    <v-col>
+                    <v-col
+                    cols="12"
+                    md="4">
                         <v-text-field
-                        label="LAST NAME"
-                        variant="outlined">
-                        </v-text-field>
+                        label="MIDDLE NAME"
+                        variant="outlined"
+                        ></v-text-field>
                     </v-col>
+               </v-row>
+
+               <v-row>
+                  
+                    
                </v-row>
             </v-container>
         </v-main>
