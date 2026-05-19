236a508293d306050cc9cadef16d96baa3bbcfe0
unknown <colisliezelann2022@gmail.com>
2026-05-19 09:34:54 +0800

    layouts and pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,21 @@
     <v-app>
         <v-main>
             <v-container>
-               
+               <v-row>
+                    <v-col>
+                        <v-text-field
+                        label="STUDENT NO."
+                        variant="outlined">
+                        </v-text-field>
+                    </v-col>
+
+                    <v-col>
+                        <v-text-field
+                        label="LAST NAME"
+                        variant="outlined">
+                        </v-text-field>
+                    </v-col>
+               </v-row>
             </v-container>
         </v-main>
     </v-app>
